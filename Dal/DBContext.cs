5dcc69905e5a177891e1e9f94f23d5da8e052da7
john.sabharwal <john.sabharwal@lngconsultancy.co.in>
2019-12-05 22:24:20 +0530

    Add project files.

---
+++ Dal/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dal
+{
+    public class DBContext: DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+        public DbSet<Users> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-0V0QKG7G\SQLEXPRESS;Database=Shopping;Trusted_Connection=True;");
+        }
+    }
+}
